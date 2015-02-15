ce7cc268dfbfd096be2b30170748fa9bb3b0c486
kkomaz <leeajl006@gmail.com>
2015-02-14 22:22:53 -0500

    get player info

--- basketball_scrape.rb
+++ basketball_scrape.rb
@@ -28,10 +28,25 @@ def show(players)
   player_info(player_html)
 end
 
+#scraping player details
 def player_info(player_html)
   player_link = open(player_html)
   player_url = Nokogiri::HTML(player_link)
-  binding.pry
+  player_position = player_url.search("div.value span.full").first.text
+  player_height = player_url.search("div.titleValuePair.hgt div.value span.full").first.text
+  player_weight = player_url.search("div.titleValuePair.wgt div.value span.full").first.text
+  player_birth = player_url.search("div.titleValuePair.dob div.value").first.text
+  player_team = player_url.search("div.titleValuePair.nbaTeam div.value").first.text
+  player_college = player_url.search("div.titleValuePair.collegeTeam div.value").first.text
+  player_hometown = player_url.search("div.titleValuePair.homeTown div.value").first.text
+
+  puts "Position: #{player_position}"
+  puts "Height: #{player_height}"
+  puts "Weight: #{player_weight}"
+  puts "Date of Birth: #{player_birth}"
+  puts "NBA Team: #{player_team}" 
+  puts "School: #{player_college}"
+  puts "Hometown: #{player_hometown}"
 end
 
 puts "Welcome to the USA BASKETBALL TEAM"
